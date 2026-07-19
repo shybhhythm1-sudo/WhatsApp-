c31a7070358178811cd00cd46613161f48b3ee8a
shybhhythm1-sudo <shybhhythm1@gmail.com>
2026-07-19 13:46:04 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+// فتح التطبيق لما المستخدم يدوس على الإشعار
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  event.waitUntil(
+    self.clients.matchAll({ type: 'window' }).then((clientList) => {
+      for (const client of clientList) {
+        if ('focus' in client) return client.focus();
+      }
+      if (self.clients.openWindow) return self.clients.openWindow('./');
+    })
+  );
+});
